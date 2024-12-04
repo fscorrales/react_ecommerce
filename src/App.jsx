6535467eecfb56b6b9fbe5375c5c548ced6d2389
Fernando Corrales <corrales_fernando@hotmail.com>
2024-12-04 20:54:21 -0300

    add responsive navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,20 @@
+import { Container } from "@mui/material";
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import About from "./components/about/About";
+import Login from "./components/auth/LoginForm";
 import Error404 from "./components/errors/Error404";
 import Home from "./components/home/Home";
-import ActualPage from "./components/navigate/ActualPage";
-import Menu from "./components/navigate/Navbar";
-import Navigate from "./components/navigate/Navigate";
+import Menu from "./components/navigate/ResponsiveNavbar";
 import ProductDetails from "./components/products/ProductDetails";
 import Produts from "./components/products/ProdutsList";
 import Users from "./components/users/Users";
-import Login from "./components/auth/LoginForm";
 
 function App() {
   return (
     <>
-      <Navigate />
-      <ActualPage />
+      {/* <Navigate />
+      <ActualPage /> */}
       <Menu />
       <Routes>
         <Route path="/" element={<Home />} />
